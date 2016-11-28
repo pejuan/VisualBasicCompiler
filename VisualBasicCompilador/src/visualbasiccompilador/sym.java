30e4c1a72c71346b6edd4ad3e9e2c7fec01d9643
pejuan <jose.pejuan@unitec.edu>
2016-11-28 16:27:30 -0600

    Do while se supone que ya propaga etiquetas

--- VisualBasicCompilador/src/visualbasiccompilador/sym.java
+++ VisualBasicCompilador/src/visualbasiccompilador/sym.java
@@ -20,7 +20,7 @@ public class sym {
   public static final int TK_AND = 28;
   public static final int TK_COMMA = 40;
   public static final int TK_OR = 29;
-  public static final int TK_UNTIL = 16;
+  public static final int TK_UNTIL = 15;
   public static final int TK_AMPERSAND = 27;
   public static final int TK_ELSEIF = 9;
   public static final int TK_IF = 7;
@@ -32,7 +32,7 @@ public class sym {
   public static final int TK_GREATEREQUAL = 34;
   public static final int TK_EXIT = 44;
   public static final int TK_LOOP = 13;
-  public static final int TK_NEXT = 17;
+  public static final int TK_NEXT = 16;
   public static final int TK_STRINGVAL = 48;
   public static final int TK_RETURN = 45;
   public static final int TK_TRUE = 18;
@@ -43,7 +43,7 @@ public class sym {
   public static final int error = 1;
   public static final int TK_STRUCTURE = 22;
   public static final int TK_TO = 12;
-  public static final int TK_WHILE = 15;
+  public static final int TK_WHILE = 14;
   public static final int TK_ELSE = 10;
   public static final int TK_READ = 47;
   public static final int TK_DIVISION = 26;
@@ -54,7 +54,7 @@ public class sym {
   public static final int TK_THEN = 8;
   public static final int TK_STRING = 4;
   public static final int TK_FUNCTION = 20;
-  public static final int TK_DO = 14;
+  public static final int TK_DO = 17;
   public static final int TK_SUB = 21;
   public static final int TK_NOTEQUAL = 37;
   public static final int TK_INTEGER = 2;
@@ -74,10 +74,10 @@ public class sym {
   "TK_FOR",
   "TK_TO",
   "TK_LOOP",
-  "TK_DO",
   "TK_WHILE",
   "TK_UNTIL",
   "TK_NEXT",
+  "TK_DO",
   "TK_TRUE",
   "TK_FALSE",
   "TK_FUNCTION",
